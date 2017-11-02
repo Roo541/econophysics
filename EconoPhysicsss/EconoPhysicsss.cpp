6cd792ce8e48cbe053e83768f1dd53126399cff5
David Roundy <roundyd@physics.oregonstate.edu>
2017-11-02 12:25:32 -0700

    use multidimensional Goods and enable linux compiling

--- EconoPhysicsss/EconoPhysicsss.cpp
+++ EconoPhysicsss/EconoPhysicsss.cpp
@@ -6,19 +6,30 @@
 #include <array>
 using namespace std;
 
-float Sell(int[], float[]);
+#define NUM_GOODS 5
+#define NUM_AGENTS 7
+
+float Sell(int[NUM_AGENTS][NUM_GOODS], float[]);
+
 int main()
 {
-	int Goods[5] = { 1,2,3,4,5 };
-	float Cash[1] = { 1000 };
+	int Goods[NUM_AGENTS][NUM_GOODS];
+	for (int a=0;a<NUM_AGENTS;a++) {
+		for (int g=0;g<NUM_GOODS;g++) {
+			Goods[a][g] = 100;
+		}
+	}
+	float Cash[NUM_AGENTS] = { 1000, 1000, 200, 10, 10, 10, 10 };
 	int number = Sell(Goods, Cash);
 	cout << number << endl;
 
+#ifdef _WIN32
 	system("Pause");
+#endif
 	return 0;
 }
 
-float Sell(int a[5], float b[1])
+float Sell(int a[NUM_AGENTS][NUM_GOODS], float b[NUM_AGENTS])
 {
 	cout << a[0] << endl;
 	cout << b[0] << endl;
