91b1c1efce03b652401c71d86945c4225bd9b7fb
Lahiru Kasun <fernandl@oregonstate.edu>
2017-11-02 12:03:29 -0700

    created function

--- EconoPhysicsss/EconoPhysicsss.cpp
+++ EconoPhysicsss/EconoPhysicsss.cpp
@@ -2,10 +2,26 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <array>
+using namespace std;
 
+float Sell(int[], float[]);
 int main()
 {
-  printf("hello world\n");
-  return 0;
+	int Goods[5] = { 1,2,3,4,5 };
+	float Cash[1] = { 1000 };
+	int number = Sell(Goods, Cash);
+	cout << number << endl;
+
+	system("Pause");
+	return 0;
 }
 
+float Sell(int a[5], float b[1])
+{
+	cout << a[0] << endl;
+	cout << b[0] << endl;
+
+	return 0;
+}
