0eb039790c19c9e136126cdc407f7eec1d71c330
Lahiru Kasun <fernandl@oregonstate.edu>
2017-11-16 12:17:25 -0800

    changes goods typed to float

--- EconoPhysicsss/EconoPhysicsss.cpp
+++ EconoPhysicsss/EconoPhysicsss.cpp
@@ -11,19 +11,19 @@ using namespace std;
 #define num_agents 5
 
 float Sell(int[num_agents][num_goods], float[]);
-float Agent_1(int[num_agents][num_goods], float[]);
+float Agent_1(float[num_agents][num_goods], float[]);
 
 //information of agents and what they have:goods, cash
 int main()
 {
-	int Goods[num_agents][num_goods]; //creates a 2-d array of agents and number of goods they have
+	float Goods[num_agents][num_goods]; //creates a 2-d array of agents and number of goods they have
 	for (int a=0;a<=num_agents;a++) {
 		for (int g=0;g<=num_goods;g++) {
 			Goods[a][g] = 100;
 		}
 	}
 	float Cash[num_agents] = { 1000, 1000, 200, 10, 10 }; //creates array of cash each agent has
-	int calories[num_goods] = { 100, 50, 250, 200, 300 }; // index of calories each good has
+	float calories[num_goods] = { 100, 50, 250, 200, 300 }; // index of calories each good has
 	float number = Agent_1(Goods, Cash); 
 	
 	system("Pause");
@@ -31,17 +31,17 @@ int main()
 	return 0;
 }
 
-float Agent_1(int Goods[num_agents][num_goods], float Cash[]) 
+float Agent_1(float Goods[num_agents][num_goods], float Cash[]) 
 {
-	int gmax[num_goods] = {10, 5, 20, 30, 5};
+	float gmax[num_goods] = {10, 5, 20, 30, 5};
 	float Q = 1;
 	float Q0 = 5;
 	float utility_1g = 10;
 	float utility_1Q = (Q / Q0) / ((Q / Q0) - 1);
 	float utility_1I = log(Cash[0]);
 	for (int a = 0;a <= num_goods; a++) {
-		 float placeholder = (Goods[0][a] / 5) / ((Goods[0][a] / 5) + 1);
-		 utility_1g = placeholder + (Goods[0][a] / gmax[a]) / ((Goods[0][a] / gmax[a]) + 1);
+		 float placeholder = (Goods[0][a] / gmax[a]) / ((Goods[0][a] / gmax[a])*(Goods[0][a] / gmax[a]) + 1);
+		 utility_1g = placeholder + (Goods[0][a] / gmax[a]) / ((Goods[0][a] / gmax[a])*(Goods[0][a] / gmax[a]) + 1);
 	}	
 	float Utility_1_T = utility_1Q + utility_1g + utility_1I;
 	
