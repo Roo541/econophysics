2dda10e08e14ffabe29d5664aa8d5781ec420bb2
Roo541 <33165603+Roo541@users.noreply.github.com>
2018-10-29 20:39:18 -0700

    Add files via upload

--- front_end_ai.py
+++ front_end_ai.py
@@ -9,7 +9,10 @@ import csv
 import agents
 
 #global stuff
-tmax = int(1e1)
+tmax = int(1e4)
+
+i_g = 100
+i_ba = 10000
 
 agents.my_preferences = np.array([
 	[200.,5,5,5,5],
@@ -25,9 +28,9 @@ agents.my_preferences = np.array([
 ])
 num_agents = len(agents.my_preferences)
 
-agents.my_preferences *= 0.1
-agents.goods[:] = 10
-agents.Bank_Account[:] = 100
+agents.my_preferences *= 1
+agents.goods[:] = i_g
+agents.Bank_Account[:] = i_ba
 
 agent_names = num_agents*["ia"]
 
@@ -37,33 +40,39 @@ for i in range(num_agents):
 	print '  -- ', agents.goods[i], agents.marginal_utilities(i, agents.goods[i]), agent_names[i], i, agents.Bank_Account[i], agents.my_utilities[i](i, agents.goods[i])
 print agents.Bank_Account[:num_agents]
 
-
 for i in range(num_agents):
         marginal = agents.marginal_utilities(i, agents.goods[i])
         for g in range(agents.Numgoods):
                 print 'agent', i, 'good', g, 'mu', marginal[g]/agents.box[g][3]
 
+#Saves Data onto Excel file
 myFile = open('test.csv', 'w')
 with myFile:
         writer = csv.writer(myFile, lineterminator='\n')
-        writer.writerow('Goods for each Agent')
+        writer.writerow(('Initial Conditions', 'goods = %d' %i_g, 'Bank_Account = %d' %i_ba,))
+        writer.writerow(())
+        writer.writerow(('Initial Preferences',))
+        for i in range(num_agents):
+			writer.writerow(('Agent_%d' %i, agents.my_preferences[i]))
+        writer.writerow(())
+        writer.writerow(('Goods for each Agent', 'g_0', 'g_1', 'g_2', 'g_3', 'g_4',))
         for i in range(num_agents):
                 writer.writerow(('Agent_%d' %i,  agents.goods[i][0],agents.goods[i][1], agents.goods[i][2], agents.goods[i][3], agents.goods[i][4]))
         writer.writerow(())
-        writer.writerow(('Bank Accounts'))
+        writer.writerow(('Bank Accounts', 'Dollars',))
         for i in range(num_agents):
                 writer.writerow(('Agent_%d' %i, agents.Bank_Account[i]))
         writer.writerow(())
-        writer.writerow(('Prices of Goods'))
+        writer.writerow(('Prices of Goods', 't-2', 't-1', 't', 'Avg',))
         for i in range(agents.Numgoods):
                 writer.writerow(('good_%d' %i, agents.box[i][0], agents.box[i][1], agents.box[i][2], agents.box[i][3]))
         writer.writerow(())
-        writer.writerow(('Money Utility'))
+        writer.writerow(('Money Utility', 'g_0', 'g_1', 'g_2', 'g_3', 'g_4',))
         for i in range(num_agents):
                 marginal = agents.marginal_utilities(i, agents.goods[i])
                 writer.writerow(('Agent_%d' %i, marginal[0]/agents.box[0][3], marginal[1]/agents.box[1][3], marginal[2]/agents.box[2][3], marginal[3]/agents.box[3][3], marginal[4]/agents.box[4][3],))
         writer.writerow(())
-        writer.writerow(('Marginal Utility'))
+        writer.writerow(('Marginal Utility', 'g_0', 'g_1', 'g_2', 'g_3', 'g_4',))
         for i in range(num_agents):
                 marginal = agents.marginal_utilities(i, agents.goods[i])
                 writer.writerow(('Agent_%d' %i, marginal[0], marginal[1], marginal[2], marginal[3], marginal[4]))
@@ -108,7 +117,7 @@ plt.legend()
 	# ~ plt.ylabel(r'marginal utility $\frac{\partial U}{\partial g_i}$')
 
 print agents.box
-#plt.show()
+plt.show()
 #search past 10 rounds for highest bid and lowest ask of all goods
 # ~ hi = 0
 # ~ low = 1000000
